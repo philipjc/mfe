6dfdbf96556a62c725d03ad703d78c516c04fbe4
Philip Cox <80514248+philc-homehub@users.noreply.github.com>
2022-06-06 21:04:53 +0100

    Update App.js
    Changed code in container dir to trigger GH action.

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <>
-      <h1>Container</h1>
+      <h1>Container Application</h1>
       <hr/>
       <MarketingApp />
     </>
