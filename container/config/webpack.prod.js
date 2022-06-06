4d9c26638e10000f0baf1dfe50dfff71104d01ce
Philip Cox <80514248+philc-homehub@users.noreply.github.com>
2022-06-06 20:56:02 +0100

    created container deploy script.
    create containe deploy script.

---
+++ container/config/webpack.prod.js
@@ -0,0 +1,24 @@
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('../package.json');
+
+const commonConfig = require('./webpack.common');
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+module.exports = merge(commonConfig, {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`, // this is a big assumption that these files will reside in the sub dirs.
+      },
+      shared: packageJson.dependencies,
+    }),
+  ]
+});
+
--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,11 +1,10 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
 
 const commonConfig = require('./webpack.common');
 
-const devConfig = {
+module.exports = merge(commonConfig,{
   mode: 'development',
   devServer: {
     port: 8081,
@@ -21,11 +20,6 @@ const devConfig = {
         './MarketingApp': './src/bootstrap.js',
       },
       shared: packageJson.dependencies
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
     })
   ]
-}
-
-module.exports = merge(commonConfig, devConfig);
+});
