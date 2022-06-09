dc2effd5c61aaf23e26c259ade6641e289de9e01
Philip Cox <80514248+philc-homehub@users.noreply.github.com>
2022-06-09 14:16:25 +0100

    small update enable a push to build.

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <>
-      <h1>Container Application with Github Actions</h1>
+      <h1>Container Application with Actions</h1>
       <hr/>
       <MarketingApp />
     </>
