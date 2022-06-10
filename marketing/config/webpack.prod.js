93657484c0c1b24c828bfe269db9a6ef009837b3
Philip Cox <80514248+philc-homehub@users.noreply.github.com>
2022-06-10 11:42:02 +0100

    public path to s3 bucket for marketing

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -7,7 +7,8 @@ const commonConfig = require('./webpack.common');
 module.exports = merge(commonConfig,{
   mode: 'production',
   output: {
-    filename: '[name].[contenthash].js'
+    filename: '[name].[contenthash].js',
+    publicPath: '/marketing/latest/' // adds the extra cloud-front dir to path.
   },
   plugins: [
     new ModuleFederationPlugin({
