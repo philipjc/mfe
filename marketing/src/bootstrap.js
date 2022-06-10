cf148902909bbb6d4d0858f029fd8ea44c2f2450
Philip Cox <80514248+philc-homehub@users.noreply.github.com>
2022-06-10 09:04:30 +0100

    same

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 const mount = el => {
-  console.log('mounting marketing..')
+  console.log('mounting marketing.')
   ReactDOM.render(
     <App />,
     el,
