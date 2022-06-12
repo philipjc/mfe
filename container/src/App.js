45a548b8a917e4b5f917cc2ad7c94627edbfe5d3
Philip Cox <80514248+philc-homehub@users.noreply.github.com>
2022-06-12 13:00:03 +0100

    added unique class generator.

--- container/src/App.js
+++ container/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 import MarketingApp from "./components/MarketingApp";
 import Header from './components/Header';
 
+const generateClassNames = createGenerateClassName({
+  productionPrefix: 'pa',
+});
+
 export default () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <MarketingApp />
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassNames}>
+      <BrowserRouter>
+        <Header />
+        <MarketingApp />
+      </BrowserRouter>
+    </StylesProvider>
   )
 };
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+const generateClassNames = createGenerateClassName({
+  productionPrefix: 'ma',
+});
+
 export default () => {
   return (
-    <StylesProvider>
+    <StylesProvider generateClassName={generateClassNames}>
       <BrowserRouter>
         <Switch>
           <Route path="/" component={Landing} />
